e8c0ed71179e39caa40d0bce208b13ea9428f161
searching-block-4535 <juancmezat@gmail.com>
2020-10-31 08:24:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(str)
   words = str.split(" ")
-  words.collect do |word|
+  words2 = words.collect do |word|
     new_word = []
     for i in 0...word.length
       new_word.unshift(word[i])
@@ -9,5 +9,5 @@ def reverse_each_word(str)
     new_word = new_word.join("")
     word = new_word
   end
-  words.join(" ")
+  words2.join(" ")
 end     

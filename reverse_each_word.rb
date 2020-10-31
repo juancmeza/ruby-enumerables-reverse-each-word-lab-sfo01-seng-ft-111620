1d927df38b3c2ded1f5f4477c88aef6152cea21d
searching-block-4535 <juancmezat@gmail.com>
2020-10-31 08:25:02 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+require 'pry'
+def reverse_each_word(str)
+  words = str.split(" ")
+  words2 = words.collect do |word|
+    new_word = []
+    for i in 0...word.length
+      new_word.unshift(word[i])
+    end
+    new_word = new_word.join("")
+    word = new_word
+  end
+  words2.join(" ")
+end     

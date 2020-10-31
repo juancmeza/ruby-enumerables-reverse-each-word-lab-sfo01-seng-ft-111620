2b954c0db4040d567447a8d1d4cf2901ed336922
searching-block-4535 <juancmezat@gmail.com>
2020-10-31 08:19:01 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+require 'pry'
+def reverse_each_word(str)
+  words = str.split(" ")
+  words.collect do |word|
+    new_word = []
+    for i in 0...word.length
+      new_word.unshift(word[i])
+    end
+    new_word = new_word.join("")
+    new_word
+  end
+  binding.pry
+  words = words.join(" ")
+end     
